c10d60f7906a74f555905ba55596f3cc6c5c3065
Nawaf1w3 <57842047+Nawaf1w3@users.noreply.github.com>
2024-04-27 15:16:54 +0200

    new backgrounds + pluse sections are coming from the same file in every page (included from that file )

--- about.php
+++ about.php
@@ -24,36 +24,21 @@
                </div>
             </div>
          </div>
-      </div>
+     
       <!-- header section end -->
       <!-- about sectuion start -->
-      <div class="about_section layout_padding pb-4">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-6">
-                  <h1 class="about_taital">About Us</h1>
-                  <p class="about_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All </p>
-                  <div class="read_bt_1"><a href="#">Read More</a></div>
-               </div>
-               <div class="col-md-6">
-                  <div class="about_img">
-                     <div class="video_bt">
-                        <div class="play_icon"><img src="images/play-icon.png"></div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
+      <?php
+         include ('sections/about.html');
+      ?>
       <!-- about sectuion end -->
  
 
       <!-- Javascript files-->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <!-- <script src="js/jquery-3.0.0.min.js"></script> -->
-      <!-- <script src="js/plugin.js"></script> -->
+         <script src="js/jquery.min.js"></script>
+         <script src="js/popper.min.js"></script>
+         <script src="js/bootstrap.bundle.min.js"></script>
+         <!-- <script src="js/jquery-3.0.0.min.js"></script> -->
+         <!-- <script src="js/plugin.js"></script> -->
    </body>
 
       <?php

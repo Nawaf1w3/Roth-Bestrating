c28e3e0ed5ca7b787ab866f73a78c882d3f147bd
Nawaf1w3 <57842047+Nawaf1w3@users.noreply.github.com>
2024-05-10 00:07:17 +0200

    ss

--- services.php
+++ services.php
@@ -8,24 +8,102 @@
          <div class="overlay"></div>
             <div id="my_slider" class="carousel slide" >
                <div class="carousel-inner">
-                  <div class="carousel-item active">
+                  <div class="slide-item active">
                      <div class="container">
                         <div class="row">
                            <div class="col-sm-12">
                               <div class="banner_taital_main">
                                  <h1 class="banner_taital">
-                                    Our Services
+                                    Bestrating
                                  </h1>
-                                 <p class="banner_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+                                 <p class="banner_text">Bij Bestratingsbedrijf Roth begrijpen we dat de juiste bedrijfsbestrating essentieel is voor een professionele uitstraling en optimale functionaliteit van uw bedrijfsomgeving. </p>
                               </div>
+                              <div class="btn_main">
+                                    <div class="started_text active"><a href="Bestrating.php">Contact US</a></div>
+                                    <div class="started_text"><a href="#section_about">About Us</a></div>
+                                 </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="slide-item ">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-sm-12">
+                              <div class="banner_taital_main">
+                                 <h1 class="banner_taital">
+                                 TUIN AANLEG / SIERBESTRATING
+                                 </h1>
+                                 <p class="banner_text"> Onze tuinaanlegdiensten combineren vakmanschap en creativiteit om uw droomtuin tot leven te brengen. Met hoogwaardige sierbestrating als kern van ons aanbod, streven we ernaar om jouw buitenruimte te transformeren tot een waar paradijs. </p>
+                              </div>
+                              <div class="btn_main">
+                                    <div class="started_text active"><a href="tuinbestratin.php">meer weten</a></div>
+                                    <div class="started_text"><a href="#section_about">About Us</a></div>
+                                 </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="slide-item ">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-sm-12">
+                              <div class="banner_taital_main">
+                                 <h1 class="banner_taital">
+                                 Grondwerk
+                                 </h1>
+                                 <p class="banner_text"> Professionele Grondwerkzaamheden en Zandlevering" Welkom bij Bestratingsbedrijf Roth, uw partner voor professionele grondwerkzaamheden en zandlevering </p>
+                              </div>
+                              <div class="btn_main">
+                                    <div class="started_text active"><a href="grondwerk.php">Contact US</a></div>
+                                    <div class="started_text"><a href="#section_about">About Us</a></div>
+                                 </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="slide-item ">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-sm-12">
+                              <div class="banner_taital_main">
+                                 <h1 class="banner_taital">
+                                 MATERIALEN LEVEREN
+                                 </h1>
+                                 <p class="banner_text"> Bij Bestratingsbedrijf Roth zijn we gepassioneerd over het creëren van prachtige buitenruimtes die zowel functioneel als esthetisch zijn. Met trots leveren we een breed scala aan hoogwaardige materialen en diensten om aan al jouw bestratingsbehoeften te voldoen. </p>
+                              </div>
+                              <div class="btn_main">
+                                    <div class="started_text active"><a href="material.delivery.php">Contact US</a></div>
+                                    <div class="started_text"><a href="#section_about">About Us</a></div>
+                                 </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="slide-item ">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-sm-12">
+                              <div class="banner_taital_main">
+                                 <h1 class="banner_taital">
+                                 REPIRATIE / HERBESTRATING
+                                 </h1>
+                                 <p class="banner_text">Bij Bestratingsbedrijf Roth staan we voor u klaar, of het nu gaat om het herstraten van uw oprit, tuinpaden of het uitvoeren van kleine reparaties zoals het ophogen van gedeeltes.</p>
+                              </div>
+                              <div class="btn_main">
+                                    <div class="started_text active"><a href="repiratie.php">Contact US</a></div>
+                                    <div class="started_text"><a href="#section_about">About Us</a></div>
+                                 </div>
                            </div>
                         </div>
                      </div>
                   </div>
                </div>
+               <button class="prev_slide">&lt;</button>
+               <button class="next_slide">&gt;</button>
             </div>
          </div>
-      </div>
+     
    
       <!-- header section end -->
       <!-- services section start -->
@@ -40,6 +118,60 @@
       include('footer.html');
       ?>
       <script>
+
+
+document.addEventListener("DOMContentLoaded", function() {
+         const carouselItems = document.querySelectorAll(".slide-item");
+         let currentIndex = 0;
+         let intervalId;
+
+            intervalId = setInterval(showNextSlide, 1000);
+
+         function showNextSlide() {
+            resetTimer();
+            carouselItems[currentIndex].classList.remove("active", "slide-in", "slide-in-right");
+            carouselItems[currentIndex].classList.add("slide-out-left");
+
+            const nextIndex = (currentIndex + 1) % carouselItems.length;
+
+            carouselItems[nextIndex].classList.remove("slide-out-left", "slide-out-right");
+            carouselItems[nextIndex].classList.add("active", "slide-in");
+            currentIndex = nextIndex;
+         }
+
+         function showPrevSlide() {
+            resetTimer();
+
+            carouselItems[currentIndex].classList.remove("active", "slide-in", "slide-in-right");
+            carouselItems[currentIndex].classList.add("slide-out-right");
+
+            const prevIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+
+            carouselItems[prevIndex].classList.remove("slide-out-right", "slide-out-left");
+            carouselItems[prevIndex].classList.add("active", "slide-in-right");
+
+            currentIndex = prevIndex;
+         }
+
+         function handleTransitionEnd(event) {
+
+            event.target.classList.remove("slide-out-left", "slide-out-right");
+         }
+
+         carouselItems.forEach(item => {
+            item.addEventListener("transitionend", handleTransitionEnd);
+         });
+
+
+
+         function resetTimer() {
+            clearInterval(intervalId);
+            intervalId = setInterval(showNextSlide, 7000);
+         }
+
+         document.querySelector(".prev_slide").addEventListener("click", showPrevSlide);
+         document.querySelector(".next_slide").addEventListener("click", showNextSlide);
+      });
 
       function toggleText(button) {
             const container = button.closest('.services_item').querySelector('.services_text_container');

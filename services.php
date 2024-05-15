bc8af5fd07fc8b96bd8ed10a23b2343c08d1fd74
Nawaf1w3 <57842047+Nawaf1w3@users.noreply.github.com>
2024-05-15 13:34:06 +0200

    preformance fix

--- services.php
+++ services.php
@@ -99,18 +99,18 @@
                      </div>
                   </div>
                </div>
-               <button class="prev_slide">
+               <div class="prev_slide">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="35" height="35">
                      <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
                      <path fill="none" d="M0 0h24v24H0z"/>
                   </svg>
-               </button>
-               <button class="next_slide">
+               </div>
+               <div class="next_slide">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="35" height="35">
                      <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
                      <path fill="none" d="M0 0h24v24H0z"/>
                   </svg>
-               </button>
+               </div>
             </div>
          </div>
      
